6bdda8f3e0a798fd345b77ecea28fab1feefad7f
Bernie <b4456609@gamil.com>
2017-01-23 14:45:14 +0800

    add upload to how page

--- src/HowPage.js
+++ src/HowPage.js
@@ -4,12 +4,25 @@ import {
   Row,
   Col,
 } from 'react-bootstrap';
+import Highlight from 'react-highlight';
+
+//no-useless-escape
+const uploadCode =
+`curl -i -X POST \\
+  -H "Content-Type: multipart/form-data" \\
+  -F "data=@./mpd.json" http://localhost/api/mpd`;
 
 const HowPage = () => (
   <Grid>
     <Row>
       <Col md={12}>
         <h1>Upload</h1>
+        <p>
+          Upload MPD file to server.
+        </p>
+        <Highlight className='sh'>
+          {uploadCode}
+        </Highlight>
       </Col>
     </Row>
   </Grid>
