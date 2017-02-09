9a28bdc8f396dc6b6ef275b936ea86b910b0e267
Bernie <b4456609@gmail.com>
2017-02-10 00:01:44 +0800

    how page use markdown

---
+++ public/how.md
@@ -0,0 +1,62 @@
+# Upload
+Upload MPD file to server.
+``` sh
+curl -i -X POST \\
+  -H "Content-Type: multipart/form-data" \\
+  -F "file=@./mpd.json" http://localhost/api/upload
+```
+
+# Service Test
+## Requirement
+- Java 8
+- `curl` command for rest call
+- Gradle project
+
+## Step
+- Put the following content in `build.gradle` file in service test project.
+- Configure the environment variable.
+- Run with `./gradlew configure pactverify`
+
+build.gradle
+``` groovy
+import groovy.json.JsonSlurper
+plugins {
+  id 'java'
+  id "au.com.dius.pact" version "3.3.6"
+}
+
+ext{
+  mgpUrl = "http://localhost:3000/test"
+}
+
+pact {
+   reports {
+      defaultReports() // adds the standard console output
+
+      markdown // report in markdown format
+      json // report in json format
+    }
+}
+
+task configure() {
+  def p = new URL(project.ext.mgpUrl).text
+  println p
+    
+  def json = new JsonSlurper().parseText(p)
+  // println json
+  project.pact.serviceProviders{
+    json.each{ provider ->
+      "${provider.provider}"{
+        host=System.getenv("${provider.host}")
+        port=System.getenv("${provider.port}")
+        provider.hasPactWith.each { consumer ->
+          hasPactWith("${consumer.consumer}"){
+            pactFile=url("${consumer.pactFile}")
+          }
+        }
+      }    
+    }
+  println project.pact.serviceProviders
+  }
+}
+```
--- src/HowPage.js
+++ src/HowPage.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { Component } from 'react';
+import marked from 'marked';
 import {
   Grid,
   Row,
@@ -8,24 +9,45 @@ import Highlight from 'react-highlight';
 
 //no-useless-escape
 const uploadCode =
-`curl -i -X POST \\
+  `curl -i -X POST \\
   -H "Content-Type: multipart/form-data" \\
   -F "file=@./mpd.json" http://localhost/api/upload`;
+let html = "";
 
-const HowPage = () => (
-  <Grid>
-    <Row>
-      <Col md={12}>
-        <h1>Upload</h1>
-        <p>
-          Upload MPD file to server.
-        </p>
-        <Highlight className='sh'>
-          {uploadCode}
-        </Highlight>
-      </Col>
-    </Row>
-  </Grid>
-);
+// Synchronous highlighting with highlight.js
+marked.setOptions({
+  highlight: function (code) {
+    return require('highlight.js').highlightAuto(code).value;
+  }
+});
+
+class HowPage extends Component {
+  constructor() {
+    super()
+    const self =this;
+    fetch('/how.md')
+      .then((res) => {
+        return res.text();
+      })
+      .then((res) => {
+        html =marked(res);
+        self.forceUpdate();
+      })
+  }
+
+  render() {
+    return (
+      <Grid>
+        <Row>
+          <Col md={2}></Col>
+          <Col md={8}>
+            <div dangerouslySetInnerHTML={{ __html: html }}></div>
+          </Col>
+        </Row>
+      </Grid>
+    );
+  }
+}
 
 export default HowPage;
+
