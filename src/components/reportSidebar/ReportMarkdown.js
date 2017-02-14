f87ff74294e431e40eb00b70e915d1e167732bdb
Bernie <b4456609@gmail.com>
2017-02-14 21:46:30 +0800

    add report pass label

--- src/components/reportSidebar/ReportMarkdown.js
+++ src/components/reportSidebar/ReportMarkdown.js
@@ -13,13 +13,21 @@ const Option = ({data, showReport}) => (
       <h3>Provider Report</h3>
       {
         data.map((item, i) => {
+          let result = (
+            <Label style={{ float: 'right' }} bsStyle="danger">
+              {`${item.error} error`}
+            </Label>);
+          if (item.error === 0) {
+            result = (
+              <Label style={{ float: 'right' }} bsStyle="success">
+                Pass
+            </Label>);
+          }
           return (
-            <ListGroupItem key={i} onClick={()=>{showReport('Report', item.report)}}>
+            <ListGroupItem key={i} onClick={() => { showReport('Report', item.report) }}>
               <Glyphicon style={{ color: 'grey' }} glyph="file" />
               {` ${item.provider}`}
-              <Label style={{ float: 'right' }} bsStyle="danger">
-                {`${item.error} error`}
-              </Label>
+              {result}
             </ListGroupItem>
           )
         })
