38835d462c664916558b16ccae250e1f4a04c49e
Bernie <b4456609@gmail.com>
2017-05-01 18:01:56 +0800

    modify uat display report

--- src/components/reportSidebar/UATReport.js
+++ src/components/reportSidebar/UATReport.js
@@ -5,38 +5,62 @@ import {
   ListGroupItem,
   Label,
   Glyphicon,
-  Button
+  Button,
+  Tooltip,
+  OverlayTrigger
 } from 'react-bootstrap';
-import {getCucumberReportURL} from '../../api/bdd'
+import { getCucumberReportURL } from '../../api/bdd'
 
-const UATReport = ({data, timestamp}) => (
-  <Panel collapsible defaultExpanded header="UAT Report">
-    <h3>Scenario Report</h3>
-    <ListGroup>
-      {
-        data.map((item, i) => {
-          let result = (
-            <Label style={{ float: 'right' }} bsStyle="danger">
-              {`${item.error} error`}
-            </Label>);
-          if (item.error === 0) {
-            result = (
-              <Label style={{ float: 'right' }} bsStyle="success">
-                Pass
-            </Label>);
-          }
-          return (
-            <ListGroupItem key={i}>
-              <Glyphicon style={{ color: 'grey' }} glyph="tasks" />
-              {` ${item.name}`}
-              {result}
-            </ListGroupItem>
-          )
-        })
-      }
-    </ListGroup>
-    <Button target="_blank" href={getCucumberReportURL(timestamp)} bsStyle="primary" bsSize="large">Full Report</Button>
-  </Panel>
+function getStatus(error) {
+  if (error === 0) {
+    return (
+      <Label style={{ float: 'right' }} bsStyle="success">
+        Pass
+      </Label>);
+  }
+  return (
+    <Label style={{ float: 'right' }} bsStyle="danger">
+      {`${error} error`}
+    </Label>);
+}
+
+const tooltip = (
+  <Tooltip id="tooltip">Click To View Report</Tooltip>
 );
+
+const UATReport = ({ data, timestamp }) => {
+  let max = Math.max(...data.map(i => i.runNumber));
+  let list = [];
+  console.log(max, data)
+  for (let j = 1; j <= max; j++) {
+    let section = (
+      <ListGroup key={j}>
+        <OverlayTrigger placement="left" overlay={tooltip}>
+          <ListGroupItem header={`Run Iteration ${j}`} target="_blank" href={getCucumberReportURL(timestamp) + '?index=' + (j - 1)} />
+        </OverlayTrigger>
+        {data
+          .filter((item) => item.runNumber === j)
+          .map((item, i) => {
+            let result = getStatus(item.error)
+            return (
+              <ListGroupItem key={i}>
+                <Glyphicon style={{ color: 'grey' }} glyph="tasks" />
+                {` ${item.name}`}
+                {result}
+              </ListGroupItem>
+            )
+          })}
+      </ListGroup>)
+    list.push(section)
+    // list.push(
+    //   <Button target="_blank" href={getCucumberReportURL(timestamp) + '?index=' + (j - 1)} bsStyle="primary" bsSize="large">Full Report</Button>);
+  }
+  return (
+    <Panel collapsible defaultExpanded header="UAT Report">
+      <h3>Scenario Report</h3>
+      {list}
+    </Panel>
+  )
+};
 
 export default UATReport;
